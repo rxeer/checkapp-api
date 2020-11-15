ba593f0cad2a03348e2b706fde3cdf9ff16c48b1
FreAK19 <andrey.kiri4encko@gmail.com>
2020-11-15 21:37:11 +0200

    commit

--- build/@types/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var User_1 = require("./User");
-exports.UserDto = User_1.UserDto;
-var Income_1 = require("./Income");
-exports.IncomeDto = Income_1.IncomeDto;
-var IncomeStatistics_1 = require("./IncomeStatistics");
-exports.IncomeStatisticDto = IncomeStatistics_1.IncomeStatisticDto;
