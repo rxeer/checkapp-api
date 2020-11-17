e584032d2298c743f230dedbfb9c5c10f9fe0f78
FreAK19 <andrey.kiri4encko@gmail.com>
2020-11-17 08:21:11 +0200

    remove build folder

--- build/configurations/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var morgan_1 = __importDefault(require("morgan"));
-exports.configureLogger = function (app, options) {
-    app.use(morgan_1.default('common', options));
-};
