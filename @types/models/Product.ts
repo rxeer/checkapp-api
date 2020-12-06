6b902be6c047970f9fee0bb213b10b12a96dd61a
FreAK19 <andrey.kiri4encko@gmail.com>
2020-12-06 16:47:56 +0200

    add familyGroupId to product

--- @types/models/Product.ts
+++ @types/models/Product.ts
@@ -6,6 +6,7 @@ export interface IProductInterface extends Document {
   userId: string;
   categoryId: string;
   description: string;
+  familyGroupId: string;
   createdDate: Date;
 }
 
@@ -21,12 +22,14 @@ export class ProductDto {
   public userId: string = '';
   public categoryId: string = '';
   public description: string = '';
+  public familyGroupId: string = '';
   public createdDate: Date = new Date();
 
   constructor(data?: ProductDto) {
     if (data) {
       this.count = data.count;
       this.userId = data.userId;
+      this.familyGroupId = data.familyGroupId;
       this.categoryId = data.categoryId;
       this.createdDate = data.createdDate;
       this.description = data.description;
